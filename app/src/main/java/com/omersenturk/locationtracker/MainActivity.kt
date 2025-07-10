77135708df0135549b4b39548494fff425d2d049
Omer Senturk <omersenturk.dev@gmail.com>
2025-07-10 22:41:52 +0300

    feat(MainActivity): check GPS status in onCreate() when the app launches

--- app/src/main/java/com/omersenturk/locationtracker/MainActivity.kt
+++ app/src/main/java/com/omersenturk/locationtracker/MainActivity.kt
@@ -8,8 +8,7 @@ import android.os.Bundle
 import android.provider.Settings
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     val locationChangeReceiver = LocationChangeReceiver()
@@ -17,11 +16,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        checkLocationService()
+
     }
 
     override fun onResume() {
