e9e17a6d693b6c20246c52e164e58c8a77c67fd6
Omer Senturk <omersenturk.dev@gmail.com>
2025-07-10 22:40:30 +0300

    refactor(LocationChangeReceiver): improve LocationChangeReceiver to check GPS_PROVIDER status before calling checkLocationService()

--- app/src/main/java/com/omersenturk/locationtracker/LocationChangeReceiver.kt
+++ app/src/main/java/com/omersenturk/locationtracker/LocationChangeReceiver.kt
@@ -7,12 +7,13 @@ import android.location.LocationManager
 
 class LocationChangeReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        if(intent?.action == LocationManager.PROVIDERS_CHANGED_ACTION)
-        {
-            if (context is MainActivity)
-            {
-                context.checkLocationService()
-            }
+        val locationManager = context?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val isLocationEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        if(intent?.action != LocationManager.PROVIDERS_CHANGED_ACTION) return
+        if(context !is MainActivity) return
+
+        if(isLocationEnabled.not()){
+            context.checkLocationService()
         }
     }
 }
